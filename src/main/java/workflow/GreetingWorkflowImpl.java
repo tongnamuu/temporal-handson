f68b1209bc1e38085b67ec3d7e64e9e0e2332689
tongnamuu <thban33@naver.com>
2026-09-25 02:36:05 +0900

    Workflow 처리 로그 추가

--- src/main/java/workflow/GreetingWorkflowImpl.java
+++ src/main/java/workflow/GreetingWorkflowImpl.java
@@ -1,8 +1,16 @@
 package workflow;
 
+import io.temporal.workflow.Workflow;
+import org.slf4j.Logger;
+
 public class GreetingWorkflowImpl implements GreetingWorkflow {
+    private static final Logger log = Workflow.getLogger(GreetingWorkflowImpl.class);
+    private static final long WORKER_PID = ProcessHandle.current().pid();
+
     @Override
     public GreetingResult greet(GreetingInput input) {
+        log.info("Greeting 처리: workerPid={}, workflowId={}",
+                WORKER_PID, Workflow.getInfo().getWorkflowId());
         return new GreetingResult(
                input.name() + " result"
         );
