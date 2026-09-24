3bb36d51cf033ce347a5da81de2d101da7296c01
tongnamuu <thban33@naver.com>
2026-09-25 01:06:12 +0900

    환경설정 java 21 & temporal dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,9 +10,12 @@ repositories {
 }
 
 dependencies {
+    implementation("io.temporal:temporal-sdk:1.36.0")
+    runtimeOnly("org.slf4j:slf4j-simple:1.7.36")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
 }
 
 tasks.test {
